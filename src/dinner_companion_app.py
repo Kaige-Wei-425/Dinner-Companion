45d615ad42033e328f4b4dbce3c2ec408240914c
Kaige_Wei <89125519+Kaige-Wei-425@users.noreply.github.com>
2023-12-24 02:11:36 +1100

    add prompts based on different recipe categories and create add_rcp() function

--- src/dinner_companion_app.py
+++ src/dinner_companion_app.py
@@ -22,16 +22,35 @@ class DinnerCompainApp:
         while self.running:
             try:
                 choice = int(self.ui.display_mainMenu())
-                
             except ValueError:
                 print("\n******************************************")
                 print("* Invalid input! Please enter a integer! *")
                 print("******************************************\n")
             else:
+                # Check whether the user input is numerical and in range (1, 6)
                 if DataValidator.is_valid_int(choice, 1, 6):
                     match choice:
+                        # View all recipes
                         case 1:
                             print("1")
+
+                        # Add recipe
+                        case 2:
+                            self.add_rcp()
+                        
+                        # Search recipe
+                        case 3:
+                            pass
+                        
+                        # Update recipe
+                        case 4:
+                            pass
+                        
+                        # Delete recipe
+                        case 5:
+                            pass
+                        
+                        # Exit the app
                         case 6:
                             exit()
                 else:
@@ -40,7 +59,47 @@ class DinnerCompainApp:
                     print("*****************************************\n")
                 
 
-
+    def add_rcp(self):
+        is_add = True
+        while is_add:
+            try:
+                choice = int(self.ui.display_category())
+            except ValueError:
+                print("\n******************************************")
+                print("* Invalid input! Please enter a integer! *")
+                print("******************************************\n")
+            else:
+                if DataValidator.is_valid_int(choice, 1, 4):
+                    match choice:
+                        # Add main course
+                        case 1:
+                            title, ingredients, instruction, protein = self.ui.prompt_main_course()
+                            # Create a main course object
+                            recipe = MainCourse(title, ingredients, instruction, protein)
+                            # Add it into the database
+                            self.database.add_recipe(recipe)
+                        
+                        # Add side dish
+                        case 2:
+                            title, ingredients, instruction, calorie = self.ui.prompt_side_dish()
+                            # Create a side dish object
+                            recipe = SideDish(title, ingredients, instruction, calorie)
+                            # Add it into the database
+                            self.database.add_recipe(recipe)
+                        
+                        # Add dessert
+                        case 3:
+                            title, ingredients, instruction, sweetness_level = self.ui.prompt_dessert()
+                            # Create a dessert object
+                            recipe = Dessert(title, ingredients, instruction, sweetness_level)
+                            # Add it into the database
+                            self.database.add_recipe(recipe)
+                        case 4:
+                            is_add = False
+                else:
+                    print("\n*****************************************")
+                    print("* Please choose your option from 1 to 4!*")
+                    print("*****************************************\n")
 
 
 # Entry point of the app
--- src/user_interface.py
+++ src/user_interface.py
@@ -26,3 +26,42 @@ class UserInterface:
 
         return input(">_: ")
 
+    # Display the category of the recipe
+    def display_category(self):
+        print("Which type of recipe would you like to add?\n")
+        print("> 1. Main Course")
+        print("> 2. Side Dish")
+        print("> 3. Dessert")
+        print("> 4. Back to Main Menu --> ")
+
+        return input(">_: ")
+    
+    # Prompt user enter the main course
+    def prompt_main_course(self):
+        print("Please enter the details of the recipe")
+        title = input("> Title:\n")
+        ingredients = input("> Ingredients:\n")
+        instruction = input("> Instruction:\n")
+        protein = input("> Protein:\n")
+
+        return title, ingredients, instruction, protein
+    
+    # Prompt user enter the side dish
+    def prompt_side_dish(self):
+        print("Please enter the details of the recipe")
+        title = input("> Title:\n")
+        ingredients = input("> Ingredients:\n")
+        instruction = input("> Instruction:\n")
+        calorie = input("> Calorie:\n")
+
+        return title, ingredients, instruction, calorie
+    
+    # Prompt user enter the dessert
+    def prompt_dessert(self):
+        print("Please enter the details of the recipe")
+        title = input("> Title:\n")
+        ingredients = input("> Ingredients:\n")
+        instruction = input("> Instruction:\n")
+        sweetness_level = input("> Sweetness Level:\n")
+
+        return title, ingredients, instruction, sweetness_level
